a95b030f72b18d22e0210440e7b6a8a0fe0f1113
Mohana Misra <iammohanamisra@gmail.com>
2024-05-22 10:42:49 +0530

    Making project layout

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className = "dashboard-container">
+            <div className = "main-sidebar">
+
+            </div>
+            <div className = "main">
+                <div className="header">
+
+                </div>
+                <div className="content">
+                    <div className="project-details">
+
+                    </div>
+                    <div className="right-sidebar">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;

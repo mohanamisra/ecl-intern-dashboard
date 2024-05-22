dad0970644c648bdd05e9cf044921bebd4003976
Mohana Misra <iammohanamisra@gmail.com>
2024-05-22 12:09:42 +0530

    Creating pages

---
+++ src/pages/ProjectGoals/ProjectGoals.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProjectGoals = () => {
+    return (
+        <div>
+            PROJECT GOALS
+        </div>
+    );
+};
+
+export default ProjectGoals;
---
+++ src/pages/YourProfile/YourProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const YourProfile = () => {
+    return (
+        <div>
+            YOUR PROFILE
+        </div>
+    );
+};
+
+export default YourProfile;

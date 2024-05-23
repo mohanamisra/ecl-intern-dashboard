4ba768c409a35339a6bf8e684d9f07ad1b9c5201
Mohana Misra <iammohanamisra@gmail.com>
2024-05-23 10:47:43 +0530

    Project goals page layout

--- src/pages/ProjectGoals/ProjectGoals.jsx
+++ src/pages/ProjectGoals/ProjectGoals.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import './ProjectGoals.css'
+import Sidebar from "../../components/Sidebar/Sidebar.jsx";
+import Header from "../../components/Header/Header.jsx";
 
 const ProjectGoals = () => {
     return (
-        <div>
-            PROJECT GOALS
+        <div className='project-goals-container'>
+            <Sidebar/>
+            <div className="main">
+                <Header/>
+            </div>
+
         </div>
     );
 };

aaa0dc01d4369e9a84310c78a8926b9519f71808
Mohana Misra <iammohanamisra@gmail.com>
2024-05-23 10:55:06 +0530

    Logout page layout done

--- src/pages/LogOut/LogOut.jsx
+++ src/pages/LogOut/LogOut.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import './LogOut.css';
+import Sidebar from "../../components/Sidebar/Sidebar.jsx";
+import Header from "../../components/Header/Header.jsx";
 
 const LogOut = () => {
     return (
         <div className = 'logout-container'>
-            LOGOUT
+            <Sidebar/>
+            <div className="main">
+                <Header/>
+                <div className="content-section">
+
+                </div>
+            </div>
         </div>
     );
 };

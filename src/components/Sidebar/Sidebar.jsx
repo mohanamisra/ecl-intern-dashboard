ebad78f8a2b9242c5571eae973f2533e8b6450ca
Mohana Misra <iammohanamisra@gmail.com>
2024-05-23 11:13:41 +0530

    Sidebar styling

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import "./Sidebar.css"
 import {Link} from "react-router-dom";
+import DashboardIcon from '@mui/icons-material/Dashboard';
 
 const Sidebar = () => {
     return (
         <div className="sidebar-container">
-            <h1 className="sidebar-heading">Intern dashboard</h1>
+            <div className = "sidebar-heading">
+                <DashboardIcon id = 'dashboard-icon'/>
+                <h1>Dashboard</h1>
+            </div>
                 <ul>
-                    <li><Link to = "/home">Home</Link></li>
-                    <li><Link to = "/projectgoals">Project Goals</Link></li>
-                    <li><Link to= "/feedback">Feedback</Link></li>
-                    <li><Link to = "/yourprofile">Your Profile</Link></li>
-                    <li><Link to = "/logout">Log Out</Link></li>
+                    <li><Link to = "/home" className = 'Link'>Home</Link></li>
+                    <li><Link to = "/projectgoals" className = 'Link'>Project Goals</Link></li>
+                    <li><Link to= "/feedback" className = 'Link'>Feedback</Link></li>
+                    <li><Link to = "/yourprofile" className = 'Link'>Your Profile</Link></li>
+                    <li><Link to = "/logout" className = 'Link'>Log Out</Link></li>
                 </ul>
         </div>
     );

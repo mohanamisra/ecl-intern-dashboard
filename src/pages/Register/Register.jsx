d84438f7d20288b68b137e2cd215580150cda912
Mohana Misra <iammohanamisra@gmail.com>
2024-05-27 12:34:42 +0530

    Basic register page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -7,25 +7,46 @@ import './Register.css'
 const Register = () => {
 
     const handleLoginClick = () => {
-        console.log("LOGIN");
+        console.log("REGISTER");
     }
     return (
-        <div className = 'login-container'>
+        <div className = 'register-container'>
             <div className="general-header">
             </div>
 
-            <form action="" className="login-form">
-                <h1 className = 'form-heading'>Intern Login</h1>
-                <div className = 'form-row'>
+            <form action="" className="register-form">
+                <h1 className='form-heading'>Intern Register</h1>
+                <div className='form-row'>
                     <label htmlFor="name">Name: </label>
-                    <TextField label = "Enter name..." required = {true} size = 'small'/>
+                    <TextField label="Enter name... " required={true}
+                               size='small' id = "name"/>
+                </div>
+                <div className='form-row'>
+                    <label htmlFor="email">Email: </label>
+                    <TextField label="Enter email... " required={true}
+                               size='small' type="email" id = "email"/>
+                </div>
+                <div className='form-row'>
+                    <label htmlFor="institute">Institute: </label>
+                    <TextField label="Enter institute name... " required={true}
+                               size='small' id = "institute"/>
+                </div>
+                <div className='form-row'>
+                    <label htmlFor="project-name">Project Name: </label>
+                    <TextField label="Enter project name... " required={true}
+                               size='small' id = "project-name"/>
                 </div>
                 <div className='form-row'>
                     <label htmlFor="password">Password: </label>
-                    <Password/>
+                    <Password id = "password"/>
                 </div>
-                <Button text = "Login" buttonClass="login button" clickHandler={handleLoginClick}/>
-                <p>Forgot Password?</p>
+                <div className='form-row'>
+                    <label htmlFor="confirm-password">Confirm Password: </label>
+                    <Password id = "confirm-password"/>
+                </div>
+                <Button text="Register" buttonClass="register button"
+                        clickHandler={handleLoginClick}/>
+                <p>Account already exists?</p>
             </form>
         </div>
     );

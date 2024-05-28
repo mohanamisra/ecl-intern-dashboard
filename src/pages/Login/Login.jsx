368a9a3ae64233d509656171e6d1c0f4398609b3
Mohana Misra <iammohanamisra@gmail.com>
2024-05-28 12:14:52 +0530

    Set up login successfully

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,16 +4,31 @@ import Button from "../../components/Button/Button.jsx"
 import {useState} from "react";
 import './Login.css'
 import {Link} from "react-router-dom";
+import {signInWithEmailAndPassword} from "firebase/auth"
+import {auth} from "../../components/firebase.jsx";
 
 const Login = () => {
 
     const [email, setEmail] = useState('');
     const [newEmail, setNewEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
 
-    const handleLoginClick = () => {
+    const handlePasswordChange = (e) => {
+        setNewPassword(e.target.value);
+    }
+
+    const handleLogin = async(e) => {
+        e.preventDefault();
         setEmail(newEmail);
-        console.log(email);
+        setPassword(newPassword);
+
+        try {
+            await signInWithEmailAndPassword(auth, newEmail, newPassword);
+            window.location.href = "/home";
+        }catch(error) {
+            console.log(error.message);
+        }
     }
 
     const handleEmailChange = (e) => {
@@ -25,7 +40,7 @@ const Login = () => {
             <div className="general-header">
             </div>
 
-            <form action="" className="login-form">
+            <form action="" className="login-form" onSubmit = {handleLogin}>
                 <h1 className = 'form-heading'>Intern Login</h1>
                 <div className = 'form-row'>
                     <label htmlFor="email">Email: </label>
@@ -35,9 +50,9 @@ const Login = () => {
                 </div>
                 <div className='form-row'>
                     <label htmlFor="password">Password: </label>
-                    <Password/>
+                    <Password id = "password" handlePasswordChange={handlePasswordChange}/>
                 </div>
-                <Button text = "Login" buttonClass="login button" clickHandler={handleLoginClick}/>
+                <Button text = "Login" buttonClass="login button"/>
                 <p>Forgot Password?</p>
                 <p><Link to = "/register">New user?</Link></p>
             </form>

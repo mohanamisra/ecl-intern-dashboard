9886a2e94e4f3e6c58bce48358f446e86af33084
Mohana Misra <iammohanamisra@gmail.com>
2024-05-30 11:19:37 +0530

    Modified sidebar styling

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,17 +6,17 @@ import DashboardIcon from '@mui/icons-material/Dashboard';
 const Sidebar = () => {
     return (
         <div className="sidebar-container">
-            <div className = "sidebar-heading">
-                <DashboardIcon id = 'dashboard-icon'/>
+            <div className="sidebar-heading">
+                <DashboardIcon id='dashboard-icon' />
                 <h1>Dashboard</h1>
             </div>
-                <ul>
-                    <li><Link to = "/home" className = 'Link'>Home</Link></li>
-                    <li><Link to = "/projectgoals" className = 'Link'>Project Goals</Link></li>
-                    <li><Link to= "/feedback" className = 'Link'>Feedback</Link></li>
-                    <li><Link to = "/yourprofile" className = 'Link'>Your Profile</Link></li>
-                    <li><Link to = "/logout" className = 'Link'>Log Out</Link></li>
-                </ul>
+            <ul>
+                <li><Link to="/home" className='Link'>Home</Link></li>
+                <li><Link to="/projectgoals" className='Link'>Project Goals</Link></li>
+                <li><Link to="/feedback" className='Link'>Feedback</Link></li>
+                <li><Link to="/yourprofile" className='Link'>Your Profile</Link></li>
+                <li><Link to="/logout" className='Link'>Log Out</Link></li>
+            </ul>
         </div>
     );
 };

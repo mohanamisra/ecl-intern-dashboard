6e5bda233b4870fc1cc275ed0a36eec0ca93ca59
Mohana Misra <iammohanamisra@gmail.com>
2024-06-03 13:19:41 +0530

    Toggle task working

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import './TaskCard.css'
 
 const TaskCard = ({id, name, status, onDelete, onDone}) => {
     const [done, setDone] = useState(status);
 
+    useEffect(() => {
+        setDone(status);
+    }, [status]);
+
     const handleCheckBoxChange = () => {
         console.log("Done task ", id);
         onDone(id);
+        setDone(!done);
     }
 
     const handleDeleteTask = () => {
@@ -18,9 +23,7 @@ const TaskCard = ({id, name, status, onDelete, onDone}) => {
     return (
         <div className = {`task-card ${done? 'checked': 'not-checked'}`}>
             <div className='task-top'>
-                {status === true ?
-                    <input type="checkbox" className='checkbox' checked onChange = {handleCheckBoxChange}/> :
-                    <input type="checkbox" className='checkbox' onChange={handleCheckBoxChange}/>}
+                    <input type="checkbox" className='checkbox' checked = {done} onChange = {handleCheckBoxChange}/>
                 <p className='task-name'>{name}</p>
             </div>
             <button className = 'delete-button' onClick = {handleDeleteTask}>Delete</button>

433cee43ac12c02cb7933e10b7e2dc3f15126a64
Mohana Misra <iammohanamisra@gmail.com>
2024-06-04 12:02:05 +0530

    Starting work on supervisor role

---
+++ src/pages/SupervisorLogin/Supervisor.jsx
@@ -0,0 +1,12 @@
+import './Supervisor.css'
+import React from 'react';
+
+const Supervisor = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Supervisor;

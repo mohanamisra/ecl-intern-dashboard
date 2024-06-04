1d59cceb20367e3728035f05b53ea2ca3669d8d2
Mohana Misra <iammohanamisra@gmail.com>
2024-06-04 10:51:13 +0530

    Added to do list to page

--- src/pages/ProjectGoals/ProjectGoals.jsx
+++ src/pages/ProjectGoals/ProjectGoals.jsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './ProjectGoals.css'
 import Sidebar from "../../components/Sidebar/Sidebar.jsx";
 import Header from "../../components/Header/Header.jsx";
+import ToDoList from "../../components/ToDoList/ToDoList.jsx";
+import {auth, db} from "../../components/firebase.jsx"
+import {doc, getDoc} from "firebase/firestore"
 
 const ProjectGoals = () => {
+
+    const [userDetails, setUserDetails] = useState(null);
+    const [userId, setUserId] = useState('');
+
+    const fetchUserData = async() => {
+        auth.onAuthStateChanged((async(user) => {
+            const docRef = doc(db, "Users", user.uid);
+            const reqDoc = await getDoc(docRef);
+            if(reqDoc.exists()) {
+                setUserDetails(reqDoc.data());
+                setUserId(user.uid);
+            }
+            else {
+                console.log("User not logged in");
+            }
+        }))
+    }
+
+    useEffect(() => {
+        fetchUserData();
+    }, []);
+
     return (
         <div className='project-goals-container'>
             <Sidebar/>
             <div className="main">
                 <Header/>
                 <div className = "content-section">
-
+                    <ToDoList userId={userId}/>
                 </div>
             </div>
 

4d5b01f48e084e44d023b24ea628e080c9c187c9
Mohana Misra <iammohanamisra@gmail.com>
2024-06-05 11:09:10 +0530

    Supervisor can see all interns

--- src/pages/SupervisorView/SupervisorView.jsx
+++ src/pages/SupervisorView/SupervisorView.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react'
+import {db} from "../../components/firebase.jsx"
+import {getDocs, collection} from "firebase/firestore"
 import './SupervisorView.css'
 
 const SupervisorView = () => {
+    const [interns, setInterns] = useState([]);
+
+    const fetchInternsList = async() => {
+        const docsRef = await getDocs(collection(db, "Users"));
+        const newInternsList = [];
+        docsRef.forEach(doc => console.log(doc.data().username));
+        docsRef.forEach(doc => {
+            const newIntern = doc.data();
+            console.log(newIntern);
+            newInternsList.push(newIntern);
+        })
+        setInterns(newInternsList);
+    }
+
+    useEffect(() => {
+        fetchInternsList();
+    },[]);
+
     return (
         <div>
-            <p>I can see all interns</p>
+            <p>All Interns</p>
+            <ul>
+                {interns.map(intern => {
+                    return (
+                        <li key = {intern.email}>{intern.username}</li>
+                    )
+                })}
+            </ul>
         </div>
     );
 };

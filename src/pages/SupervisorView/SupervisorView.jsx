77d514530177a925282c87d482676fc8cf7e61d7
Mohana Misra <iammohanamisra@gmail.com>
2024-06-05 16:55:57 +0530

    Supervisor view ui done

--- src/pages/SupervisorView/SupervisorView.jsx
+++ src/pages/SupervisorView/SupervisorView.jsx
@@ -1,42 +1,47 @@
-import React, {useState, useEffect} from 'react'
-import {db} from "../../components/firebase.jsx"
-import {getDocs, collection} from "firebase/firestore"
-import {useNavigate, useParams} from "react-router-dom";
-import './SupervisorView.css'
+import React, { useState, useEffect } from 'react';
+import { db } from "../../components/firebase.jsx";
+import { getDocs, collection } from "firebase/firestore";
+import { useNavigate, useParams } from "react-router-dom";
+import './SupervisorView.css';
 import Button from "../../components/Button/Button.jsx";
 
 const SupervisorView = () => {
     const navigate = useNavigate();
-    const {supervisorName} = useParams();
+    const { supervisorName } = useParams();
     const [interns, setInterns] = useState([]);
 
-    const fetchInternsList = async() => {
+    const fetchInternsList = async () => {
         const docsRef = await getDocs(collection(db, "Users"));
         const newInternsList = [];
         docsRef.forEach(doc => {
-            const newIntern = {...doc.data(), id: doc.id};
+            const newIntern = { ...doc.data(), id: doc.id };
             newInternsList.push(newIntern);
-        })
+        });
         setInterns(newInternsList);
-    }
+    };
 
     const handleDetailsClick = (id) => {
-        navigate(`intern/${id}`)
-    }
+        navigate(`intern/${id}`);
+    };
 
     useEffect(() => {
         fetchInternsList();
-    },[]);
+    }, []);
 
     return (
-        <div>
-            <p>All Interns</p>
-            <ul>
-                {interns.map(intern => {
-                    return (
-                        <li key = {intern.id}>{intern.username} <Button text = "View Details" buttonClass={"details-button"} clickHandler={() => handleDetailsClick(intern.id)}/></li>
-                    )
-                })}
+        <div className="supervisor-view-container">
+            <h1 className="page-heading">Interns under {supervisorName}</h1>
+            <ul className="intern-list">
+                {interns.map(intern => (
+                    <li key={intern.id} className="intern-item">
+                        <span className="intern-name">{intern.username}</span>
+                        <Button
+                            text="View Details"
+                            buttonClass="details-button"
+                            clickHandler={() => handleDetailsClick(intern.id)}
+                        />
+                    </li>
+                ))}
             </ul>
         </div>
     );

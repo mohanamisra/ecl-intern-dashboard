4e067c4d3f8c6ae6970b3ec8cb0d5c920645d936
Mohana Misra <iammohanamisra@gmail.com>
2024-06-05 12:00:30 +0530

    Can see intern details and goals

--- src/pages/InternDetails/InternDetails.jsx
+++ src/pages/InternDetails/InternDetails.jsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {useParams} from "react-router-dom";
+import {db} from "../../components/firebase.jsx";
+import {doc, getDoc} from "firebase/firestore";
+import ToDoList from "../../components/ToDoList/ToDoList.jsx";
 import './InternDetails.css'
 
-const InternDetails = ({internId}) => {
-    console.log(internId);
+const InternDetails = () => {
+    const {internId} = useParams();
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [institute, setInstitute] = useState('');
+    const [project, setProject] = useState('');
+    const [start, setStart] = useState('');
+    const [end, setEnd] = useState('');
+
+    const fetchInternDetails = async() => {
+        const internRef = await getDoc(doc(db, "Users", internId));
+        setName(internRef.data().username);
+        setEmail(internRef.data().email);
+        setInstitute(internRef.data().instituteName);
+        setProject(internRef.data().projectName);
+        setStart(internRef.data().startDate);
+        setEnd(internRef.data().endDate);
+    }
+
+    useEffect(() => {
+        fetchInternDetails();
+    },[])
+
     return (
-        <div>
-            this is an intern.
+        <div className = "all-details-container">
+            <div className="section intern-details">
+                <h3 className="section-heading">Intern Details</h3>
+                <p className="intern-name">{name}</p>
+                <p className="intern-name">{email}</p>
+                <p className="intern-name">{institute}</p>
+                <p className="intern-name">{project}</p>
+                <p className="intern-name">{start}</p>
+                <p className="intern-name">{end}</p>
+            </div>
+
+            <div className="section intern-history">
+                <h3 className="section-heading">Project History</h3>
+                <div>Project history goes here</div>
+            </div>
+
+            <div className="section intern-goals">
+                <h3 className="section-heading">Project Goals</h3>
+                <ToDoList userId = {internId}/>
+            </div>
         </div>
     );
 };
--- src/pages/SupervisorView/SupervisorView.jsx
+++ src/pages/SupervisorView/SupervisorView.jsx
@@ -1,24 +1,26 @@
 import React, {useState, useEffect} from 'react'
 import {db} from "../../components/firebase.jsx"
 import {getDocs, collection} from "firebase/firestore"
+import {useNavigate} from "react-router-dom";
 import './SupervisorView.css'
 import Button from "../../components/Button/Button.jsx";
 
 const SupervisorView = () => {
+    const navigate = useNavigate();
     const [interns, setInterns] = useState([]);
 
     const fetchInternsList = async() => {
         const docsRef = await getDocs(collection(db, "Users"));
         const newInternsList = [];
         docsRef.forEach(doc => {
-            const newIntern = doc.data();
+            const newIntern = {...doc.data(), id: doc.id};
             newInternsList.push(newIntern);
         })
         setInterns(newInternsList);
     }
 
-    const handleDetailsClick = () => {
-        console.log("click");
+    const handleDetailsClick = (id) => {
+        navigate(`intern/${id}`)
     }
 
     useEffect(() => {
@@ -31,7 +33,7 @@ const SupervisorView = () => {
             <ul>
                 {interns.map(intern => {
                     return (
-                        <li key = {intern.email}>{intern.username} <Button text = "View Details" buttonClass={"details-button"} clickHandler={handleDetailsClick}/></li>
+                        <li key = {intern.id}>{intern.username} <Button text = "View Details" buttonClass={"details-button"} clickHandler={() => handleDetailsClick(intern.id)}/></li>
                     )
                 })}
             </ul>

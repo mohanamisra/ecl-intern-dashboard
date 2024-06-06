9b38c12bbf7758742731f1b6495fba18096f89d3
Mohana Misra <iammohanamisra@gmail.com>
2024-06-06 13:40:56 +0530

    Feedback ui

--- src/pages/Feedback/Feedback.jsx
+++ src/pages/Feedback/Feedback.jsx
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from 'react';
-import './Feedback.css'
-import {db} from "../../components/firebase.jsx";
-import {collection, getDocs} from "firebase/firestore";
-import {auth} from "../../components/firebase.jsx"
+import React, { useState, useEffect } from 'react';
+import './Feedback.css';
+import { db } from "../../components/firebase.jsx";
+import { collection, getDocs } from "firebase/firestore";
+import { auth } from "../../components/firebase.jsx";
 import Sidebar from "../../components/Sidebar/Sidebar.jsx";
 import Header from "../../components/Header/Header.jsx";
 
@@ -10,28 +10,33 @@ const Feedback = () => {
     const [feedbacks, setFeedbacks] = useState(null);
 
     const fetchUserFeedbacks = () => {
-        auth.onAuthStateChanged((async(user) => {
-            const feedbacksRef = await getDocs(collection(db, "Users", user.uid, "feedback"));
-            const allFeedbacks = feedbacksRef.docs.map(item => ({...item.data(), id: item.id}));
-            setFeedbacks(allFeedbacks);
-        }))
-    }
+        auth.onAuthStateChanged(async (user) => {
+            if (user) {
+                const feedbacksRef = await getDocs(collection(db, "Users", user.uid, "feedback"));
+                const allFeedbacks = feedbacksRef.docs.map(item => ({ ...item.data(), id: item.id }));
+                setFeedbacks(allFeedbacks);
+            }
+        });
+    };
 
     useEffect(() => {
         fetchUserFeedbacks();
     }, []);
 
     return (
-        <div className = 'feedback-container'>
-            <Sidebar/>
-            <div className = 'main'>
-                <Header/>
-                <div className = 'content-section'>
+        <div className='feedback-container'>
+            <Sidebar />
+            <div className='main'>
+                <Header />
+                <div className='content-section'>
                     <ul>
-                        {feedbacks === null ? <p>No feedbacks yet</p>
-                            : feedbacks.map(feedback => {
-                                return (<li key = {feedback.id}>{feedback.feedback} by {feedback.supervisorName}</li>)
-                            })}
+                        {feedbacks === null ? <p>No feedbacks yet</p> :
+                            feedbacks.map(feedback => (
+                                <li key={feedback.id}>
+                                    <strong>Feedback:</strong> {feedback.feedback}<br />
+                                    <strong>By:</strong> {feedback.supervisorName}
+                                </li>
+                            ))}
                     </ul>
                 </div>
             </div>
